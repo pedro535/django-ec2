f22845571535f2cdc6e43080234e6bdd27f939ba
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2023-11-27 17:31:40 +0000

    Update settings

--- AWSauth/settings.py
+++ AWSauth/settings.py
@@ -84,6 +84,8 @@ WSGI_APPLICATION = 'AWSauth.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': DB_NAME,
         'USER': DB_USERNAME,
