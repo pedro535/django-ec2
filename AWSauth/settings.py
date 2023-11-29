51c7de88e216f2e5dfa00fe1c8232601ca8e74ba
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2023-11-29 10:24:50 +0000

    Test logs

--- AWSauth/settings.py
+++ AWSauth/settings.py
@@ -84,14 +84,14 @@ WSGI_APPLICATION = 'AWSauth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': DB_NAME,
-        # 'USER': DB_USERNAME,
-        # 'PASSWORD': DB_PASSWORD,
-        # 'HOST': DB_HOST,
-        # 'PORT': DB_PORT,
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USERNAME,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
 
@@ -131,45 +131,45 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STORAGES = {
-#     "default": {
-#         "BACKEND": "storages.backends.s3.S3Storage",
-#         "OPTIONS": {
-#             "access_key": AWS_ACCESS_KEY_ID,
-#             "secret_key": AWS_SECRET_ACCESS_KEY,
-#             "bucket_name": AWS_S3_BUCKET_NAME,
-#             "region_name": AWS_REGION_NAME,
-#             "custom_domain": AWS_S3_CUSTOM_DOMAIN,
-#             "object_parameters": AWS_S3_OBJECT_PARAMETERS,
-#         },
-#     },
-#     "staticfiles": {
-#         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-#         "OPTIONS": {
-#             "location": 'static/',
-#             "base_url": "/static/"
-#         },
-#     },
-# }
-
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STORAGES = {
     "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "BACKEND": "storages.backends.s3.S3Storage",
         "OPTIONS": {
-            "location": MEDIA_URL
+            "access_key": AWS_ACCESS_KEY_ID,
+            "secret_key": AWS_SECRET_ACCESS_KEY,
+            "bucket_name": AWS_S3_BUCKET_NAME,
+            "region_name": AWS_REGION_NAME,
+            "custom_domain": AWS_S3_CUSTOM_DOMAIN,
+            "object_parameters": AWS_S3_OBJECT_PARAMETERS,
         },
     },
     "staticfiles": {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
         "OPTIONS": {
-            "location": STATIC_URL
+            "location": 'static/',
+            "base_url": "/static/"
         },
     },
 }
+
+
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# STORAGES = {
+#     "default": {
+#         "BACKEND": "django.core.files.storage.FileSystemStorage",
+#         "OPTIONS": {
+#             "location": MEDIA_URL
+#         },
+#     },
+#     "staticfiles": {
+#         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+#         "OPTIONS": {
+#             "location": STATIC_URL
+#         },
+#     },
+# }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
